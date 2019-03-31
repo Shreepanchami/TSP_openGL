624aea50fc0011a2c777e0e97f0514414bcaecff
Matharishwa <42656532+Matharishwa@users.noreply.github.com>
2019-03-31 14:07:03 +0530

    nodenum
    nodenum

--- TSP_openGL/TSP_openGL/TSP.cpp
+++ TSP_openGL/TSP_openGL/TSP.cpp
@@ -7,23 +7,25 @@ int nodes;
 float node_x_y[100][2];
 void myinit()
 {
+	//glClearColor(1, 1, 1, 1);
+	//glClear(GL_COLOR_BUFFER_BIT);
+	//glColor3f(1, 0, 0);
 	glClearColor(1, 1, 1, 1);
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1, 0, 0);
 	gluOrtho2D(-1000, 1000, -1000, 1000);
 }
 
-void plot_point(int x, int y)
+/*void plot_point(int x, int y)
 {
 	glColor3f(1, 0, 0);
 	glBegin(GL_POINTS);
 	glVertex2i(x, y);
 	glEnd();
 	glFlush();
-}
+}*/
 
 void circ_draw(int xc, int yc, int r)
-{
+{	
 	glBegin(GL_POINTS);
 	glColor3f(1, 0, 0);
 	int t = 0;
@@ -62,11 +64,17 @@ void matrix()
 			scanf_s("%d", &costmatrix[row][column]);
 	find_nodes();
 
-
+	char n1[10];
 	for (int i = 0; i < nodes; i++)
 	{
-
+		glColor3f(0, 0, 0);
+		glRasterPos2f(node_x_y[i][0]-12, node_x_y[i][1]-18);
+		_itoa_s(i + 1, n1, 10);
+		for (int i = 0; i < strlen(n1); i++)
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, n1[i]);
+		glFlush();
 		circ_draw((int)node_x_y[i][0], (int)node_x_y[i][1], 50);
+		
 	}
 
 
@@ -74,10 +82,10 @@ void matrix()
 }
 void display()
 {
-	int choice;
-	printf("Enter 1.If u want to insert cost as Matrix OR 2.If u want to add costs using mouse");
-	scanf_s("%d", &choice);
-	if (choice == 1)
+	//int choice;
+	//printf("Enter 1.If u want to insert cost as Matrix OR 2.If u want to add costs using mouse");
+	//scanf_s("%d", &choice);
+	//if (choice == 1)
 		matrix();
 	//else
 		//usingmouse();
@@ -87,10 +95,11 @@ int main(int argc, char *argv[])
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(2000, 2000);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(1000, 1000);
+	glutInitWindowPosition(200, 200);
 	glutCreateWindow("dda");
 	myinit();
 	glutDisplayFunc(display);
 	glutMainLoop();
+	return 0;
 }
